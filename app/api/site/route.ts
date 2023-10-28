a365721f4f899ac4cb0fa484061b32bc4dcc006e
stonega <xijieyin@gmail.com>
2023-10-28 19:04:11 +0800

    fix: update site create logic

--- app/api/site/route.ts
+++ app/api/site/route.ts
@@ -1,7 +1,8 @@
 import { NextResponse, NextRequest } from "next/server";
 import { getAuth } from "@clerk/nextjs/server";
 import { prisma } from "@/lib/prisma";
-import { getDocUrl } from "@/lib/utils";
+import { getDocUrl, getSubPaths } from "@/lib/utils";
+import urlMetadata from "url-metadata";
 
 export async function GET(request: NextRequest) {
   const { userId } = getAuth(request);
@@ -20,7 +21,7 @@ export async function GET(request: NextRequest) {
     take: pageSize,
     where,
     orderBy: {
-      createAt: "desc",
+      updateAt: "desc",
     },
   });
   return NextResponse.json(data);
@@ -31,27 +32,49 @@ export async function POST(request: NextRequest) {
   if (!userId) {
     return new NextResponse("Unauthorized", { status: 401 });
   }
-  const { description, title, icon, url } = await request.json();
-  const docUrl = getDocUrl(url);
-  const where = {
-    url: docUrl,
-  };
-  const data = await prisma.site.upsert({
-    where,
-    update: {
-      description,
-      title,
-      icon,
-    },
-    create: {
+  const { url, icon } = await request.json();
+  const urls = getSubPaths(url);
+  // Check if url already existed
+  const record = await prisma.site.findFirst({
+    where: {
       userId,
-      description,
-      title,
+      OR: urls?.map((u) => ({ url: u })),
+    },
+  });
+  if (record) {
+    await prisma.site.update({
+      where: {
+        id: record.id,
+      },
+      data: {
+        updateAt: new Date(),
+      },
+    });
+    return Response.json(record, {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    });
+  }
+  // Insert new site
+  const docUrl = getDocUrl(url);
+  if (!docUrl)
+    return new NextResponse("Internal error", {
+      status: 500,
+    });
+  const metadata = await urlMetadata(docUrl);
+  const data = await prisma.site.create({
+    data: {
+      userId: userId!,
+      description: metadata.description as string,
+      title: metadata.title as string,
       icon,
       url: docUrl!,
     },
   });
-
   return Response.json(data, {
     status: 200,
     headers: {
@@ -67,7 +90,7 @@ export async function PUT(request: NextRequest) {
   if (!userId) {
     return new NextResponse("Unauthorized", { status: 401 });
   }
-  const { description, title, icon, url, id } = await request.json();
+  const { description, title, icon, id } = await request.json();
   const where = {
     id,
   };
