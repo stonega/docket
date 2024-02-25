fde6dbdde1dd3db50a57f901683b44233677a580
stonega <xijieyin@gmail.com>
2024-02-25 18:02:30 +0800

    fix: update domain

--- tests/notion.test.ts
+++ tests/notion.test.ts
@@ -1,4 +1,4 @@
-import { Notion } from "../lib/notion";
+import { Notion } from "../lib/notion/notion";
 import { expect, test } from "vitest";
 import "dotenv/config";
 
@@ -7,8 +7,10 @@ test(
   async () => {
     const accessKey = process.env.NOTION_TEST_TOKEN;
     const client = new Notion({ auth: accessKey! });
-    const complete =await client.initSync({ userId: "user_2VD2zdHmaIDyAoIKs9bwzpk4F2s" });
-    expect(complete).toBe(true)
+    const complete = await client.initialSync({
+      userId: "user_2VD2zdHmaIDyAoIKs9bwzpk4F2s",
+    });
+    expect(complete).toBe(true);
   },
   {
     timeout: 600000,
